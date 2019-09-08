1a6e635fd1c315fa38abf4b7bba3fe01d31a44a7
Will Germany <germany.william@gmail.com>
2019-09-08 17:32:16 -0400

    Added a working client

--- client/main.js
+++ client/main.js
@@ -1,39 +1,77 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const url = require('url')
-const dotenv = require('dotenv').config()
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron');
+const { format } = require('url');
+const { join } = require('path');
 
-let browserWindow = undefined
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+const isDev = process.env.NODE_ENV || 'development';
 
-function createWindow() {
-  browserWindow = new BrowserWindow({ width: 800, height: 600, webPreferences: { nodeIntegration: true } })
+const gotTheLock = app.requestSingleInstanceLock();
 
-  const displayContent = process.env.ELECTRON_DEV_URL ||
-    url.format({
-      pathname: path.join(__dirname, 'build/index.html'),
-      protocol: 'file:',
-      slashes: true
-    })
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
 
-  browserWindow.loadURL(displayContent)
-
-  browserWindow.webContents.openDevTools()
-
-  browserWindow.on('closed', () => {
-    win = null
-  })
+  // Create myWindow, load the rest of the app, etc...
+  //app.on('ready', () => {});
 }
 
-app.on('ready', createWindow)
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-app.on('window-all-closed', () => {
+  const startUrl = isDev
+    ? 'http://localhost:3000'
+    : format({
+        pathname: join(__dirname, '/../build/index.html'),
+        protocol: 'file:',
+        slashes: true
+      });
+  // and load the index.html of the app.
+  mainWindow.loadURL(startUrl);
+
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
-  if (browserWindow === null) {
-    createWindow()
+app.on('activate', function() {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
   }
-})
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
